dcd6fef026b907ed7f0771098fd4db536296f197
JingXu <991914396@qq.com>
2015-05-14 16:55:19 +0800

    增加注释

--- EFNavigationTransitionTest/PushAnimation.h
+++ EFNavigationTransitionTest/PushAnimation.h
@@ -9,6 +9,9 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+/**
+ *  继承的是UIViewControllerAnimatedTransitioning，负责切换的具体内容，就是切换中应该发生什么，自定义效果就写在了这个接口里
+ */
 @interface PushAnimation : NSObject <UIViewControllerAnimatedTransitioning>
 
 @end
