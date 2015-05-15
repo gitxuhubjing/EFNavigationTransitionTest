c70c8c999cd5e505c94375e427fd65ee8b75c077
JingXu <991914396@qq.com>
2015-05-15 11:05:17 +0800

    fix bug

--- EFNavigationTransitionTest/PopAnimation.m
+++ EFNavigationTransitionTest/PopAnimation.m
@@ -12,7 +12,7 @@
 
 - (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext
 {
-    return 0.3f;
+    return 0.5f;
 }
 
 - (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
@@ -22,8 +22,41 @@
     
     UIView *containerView = [transitionContext containerView];
     
+    /**
+     *   app ping 的动画效果
+     */
+    self.transitionContext = transitionContext;
+    [containerView addSubview:toVC.view];
+    [containerView addSubview:fromVC.view];
+    UIBezierPath *maskStartBP = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(100, 130, 1, 1)];
+    
+    CGPoint finalPoint = CGPointMake(100 - 0, 130 - CGRectGetMaxY(toVC.view.bounds));
+    CGFloat radius = sqrt((finalPoint.x * finalPoint.x) + (finalPoint.y * finalPoint.y));
+    UIBezierPath *maskFinalBP = [UIBezierPath bezierPathWithOvalInRect:CGRectInset(CGRectMake(50, 100, 100, 60), -radius, -radius)];
+    
+    CAShapeLayer *maskLayer = [CAShapeLayer layer];
+    maskLayer.path = maskStartBP.CGPath; //将它的 path 指定为最终的 path 来避免在动画完成后会回弹
+    fromVC.view.layer.mask = maskLayer;
+    
+    //创建一个关于 path 的 CABasicAnimation 动画来从 circleMaskPathInitial.CGPath 到 circleMaskPathFinal.CGPath 。同时指定它的 delegate 来在完成动画时做一些清除工作
+    CABasicAnimation *maskLayerAnimation = [CABasicAnimation animationWithKeyPath:@"path"];
+    maskLayerAnimation.fromValue = (__bridge id)(maskFinalBP.CGPath);
+    maskLayerAnimation.toValue = (__bridge id)((maskStartBP.CGPath));
+    maskLayerAnimation.duration = [self transitionDuration:transitionContext];
+//    maskLayerAnimation.timingFunction = [CAMediaTimingFunction  functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    
+    maskLayerAnimation.delegate = self;
+    
+    [maskLayer addAnimation:maskLayerAnimation forKey:@"path"];
+    
+    
+    
+    
+    
+    /**
+
 //    CGAffineTransformMakeScale(0.8, 0.8)
-    toVC.view.transform = CGAffineTransformIdentity;
+    toVC.view.transform = CGAffineTransformMakeScale(0.8, 0.8);
     toVC.view.alpha = 0.5;
     CGRect frame = [transitionContext initialFrameForViewController:fromVC];
     CGRect offScreenFrame = frame;
@@ -40,6 +73,21 @@
         fromVC.view.transform = CGAffineTransformIdentity;
         [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
     }];
+     */
+    
 }
+
+
+#pragma mark - CABasicAnimation的Delegate
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag{
+    
+    //告诉 iOS 这个 transition 完成
+    [self.transitionContext completeTransition:![self. transitionContext transitionWasCancelled]];
+    //清除 fromVC 的 mask
+    [self.transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey].view.layer.mask = nil;
+    [self.transitionContext viewControllerForKey:UITransitionContextToViewControllerKey].view.layer.mask = nil;
+    
+}
+
 
 @end
